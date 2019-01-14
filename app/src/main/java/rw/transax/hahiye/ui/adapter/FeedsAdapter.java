0932c376b49f7813592a7edb780099a1d1d13a02
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-01-14 14:13:12 +0200

    Like touch event

---
+++ app/src/main/java/rw/transax/hahiye/ui/adapter/FeedsAdapter.java
@@ -0,0 +1,87 @@
+package rw.transax.hahiye.ui.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import rw.transax.hahiye.R;
+
+public class FeedsAdapter extends RecyclerView.Adapter<FeedsAdapter.FeedsViewHolder> {
+    private Context mContext;
+
+    public FeedsAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public FeedsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item_feed_text, parent, false);
+        return new FeedsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeedsViewHolder holder, int position) {
+        holder.bindView();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    static class FeedsViewHolder extends RecyclerView.ViewHolder {
+        private ProgressBar feed_hitLevel;
+        private ImageView btn_like;
+        private int progressStatus = 0;
+        private Boolean running = true;
+        private android.os.Handler handler = new android.os.Handler();
+
+        FeedsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            feed_hitLevel = itemView.findViewById(R.id.feed_hitLevel);
+            btn_like = itemView.findViewById(R.id.btn_feed_like);
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        void bindView() {
+            btn_like.setOnTouchListener((v, event) -> {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    new Thread(() -> {
+                        while (progressStatus < 100 && running) {
+                            progressStatus++;
+                            /*
+                             * Update progressBar value
+                             */
+                            handler.post(() -> feed_hitLevel.setProgress(progressStatus));
+                            try {
+                                // add delay of 20 milliseconds.
+                                Thread.sleep(20);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    running = false;
+                }
+                return false;
+            });
+        }
+    }
+}
--- app/src/main/java/rw/transax/hahiye/ui/fragment/FeedsFragment.java
+++ app/src/main/java/rw/transax/hahiye/ui/fragment/FeedsFragment.java
@@ -1,14 +1,29 @@
 package rw.transax.hahiye.ui.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import rw.transax.hahiye.R;
+import rw.transax.hahiye.ui.adapter.FeedsAdapter;
+import rw.transax.hahiye.utils.SimpleDividerItemDecoration;
 
 public class FeedsFragment extends Fragment {
+
+    private Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getContext();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,4 +31,13 @@ public class FeedsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_feeds, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView recyclerView = view.findViewById(R.id.feedsRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        recyclerView.addItemDecoration(new SimpleDividerItemDecoration(mContext));
+        recyclerView.setAdapter(new FeedsAdapter(mContext));
+    }
 }
